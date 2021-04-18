e819c291dd2b00479b7035b4a0c39eabc1ce4d96
Dan Vingo <dan@danvingo.com>
2021-04-18 04:11:48 -0400

    put in recur helper when starting server to find a port

--- resources/clj/new/dv.fulcro_template/src/main/app/server/server.clj
+++ resources/clj/new/dv.fulcro_template/src/main/app/server/server.clj
@@ -8,17 +8,45 @@
     [{{namespace}}.server.crux-node]
     [taoensso.timbre :as log]))
 
+;(defstate http-server
+;  :start
+;  (let [pedestal-config       (::http/config config)
+;        service (merge (service/make-service-map) pedestal-config)
+;        port (::http/port pedestal-config)]
+;    (when (nil? port)
+;      (throw (Exception. "You must set a port as the environment variable PORT.")))
+;    (log/info "Final service config: ")
+;    (pprint service)
+;    (log/info "Starting server on port" port)
+;    (-> service http/create-server http/start))
+;
+;  :stop
+;  (http/stop http-server))
+
 (defstate http-server
   :start
-  (let [pedestal-config       (::http/config config)
-        service (merge (service/make-service-map) pedestal-config)
-        port (::http/port pedestal-config)]
-    (when (nil? port)
+  (let [pedestal-config (::http/config config)
+        service         (merge (service/make-service-map) pedestal-config)]
+    (when (nil? (::http/port pedestal-config))
       (throw (Exception. "You must set a port as the environment variable PORT.")))
     (log/info "Final service config: ")
     (pprint service)
-    (log/info "Starting server on port" port)
-    (-> service http/create-server http/start))
 
+    (loop [service-map service]
+      (let [port (::http/port service-map)
+            r    (try
+                   (log/info "Starting server on port" port)
+                   (-> service-map http/create-server http/start)
+                   (catch IOException _
+                     (println "EXCeption 1")
+                     (log/info "Port " port " already in use trying" (inc port) "."))
+                   (catch Exception e
+                     (println "EXCeption 2" e " type: " (type e))))]
+        (log/info "started server val: " r)
+        (when-not r
+          (when (< 10000 port))
+          (recur
+            (merge (service/make-service-map)
+              (update pedestal-config ::http/port inc)))))))
   :stop
   (http/stop http-server))
