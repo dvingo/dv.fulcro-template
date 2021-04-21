d78768219833ba727a2e8401d556cd9dd4436c92
Dan Vingo <dan@danvingo.com>
2021-04-20 23:43:49 -0400

    Update theme

--- resources/clj/new/dv.fulcro_template/src/main/app/client/ui/styles/global_styles.cljs
+++ resources/clj/new/dv.fulcro_template/src/main/app/client/ui/styles/global_styles.cljs
@@ -4,9 +4,10 @@
   {":root"                {:box-sizing "border-box"}
    "*, ::after, ::before" {:box-sizing "inherit"}
    :body
-                          {:background-color (:bg theme)
-                           :background-image (:bg-image theme)
-                           :color            (:fg theme)
+   ;; use important here to override semantic ui styles
+                          {:background-color (str (:bg theme) "!important")
+                           :background-image (str (:bg-image theme) "!important")
+                           :color            (str (:fg theme) "!important")
                            :font-size        "1.2rem"
                            :line-height      1.3
                            :font-family      "helvetica, sans-serif"}})
@@ -15,7 +16,10 @@
   {".ui.segment.ui.segment"
    {:color            fg
     :background-color container-bg}
-   ".ui.header.ui.header"
+
+   ".ui.header.ui.header,
+    .ui.form.ui.form .field>label,
+    .ui.secondary.menu .item"
    {:color fg}})
 
 (defn global-styles
